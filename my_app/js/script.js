1b6fff28a202228ae06199a310cb4317ee37e94b
TanaraCavalcante <tanaracavalcante@icloud.com>
2024-10-09 18:54:44 -0400

    getDiscs

--- my_app/js/script.js
+++ my_app/js/script.js
@@ -1,12 +1,29 @@
-//! indirizzo dell'api
-// http://localhost/php-dischi-json/database/server.php
-console.log('sono js')
-const { createApp } = Vue
 
+const { createApp } = Vue
+console.log('ciao')
   createApp({
     data() {
       return {
-        api: 'http://localhost/php-dischi-json/database/server.php',
+        apiUrl: "http://localhost/php-dischi-json/database/server.php",
+        discs: [],
       }
+    },
+    methods:{
+        //!Chiamata GET per i dischi
+        getDiscs(url){
+            axios.get(url)
+            .then((response) => {
+              console.log(response.data);
+              console.log('ciao ciao');
+          
+            })
+            .catch(function (error) {
+              console.log(error);
+            });
+        },
+        //!Mostra i dischi
+        created(){
+            this.getDiscs(this.apiUrl);
+        },
     }
   }).mount('#app')
