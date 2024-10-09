7df074cc8e1d17979669827fa27a3c1c896690d4
TanaraCavalcante <tanaracavalcante@icloud.com>
2024-10-09 19:49:39 -0400

    v-for in index

--- my_app/js/script.js
+++ my_app/js/script.js
@@ -1,29 +1,31 @@
 
 const { createApp } = Vue
-console.log('ciao')
-  createApp({
-    data() {
-      return {
-        apiUrl: "http://localhost/php-dischi-json/database/server.php",
-        discs: [],
-      }
-    },
-    methods:{
-        //!Chiamata GET per i dischi
-        getDiscs(url){
-            axios.get(url)
-            .then((response) => {
-              console.log(response.data);
-              console.log('ciao ciao');
-          
-            })
-            .catch(function (error) {
-              console.log(error);
-            });
-        },
-        //!Mostra i dischi
-        created(){
-            this.getDiscs(this.apiUrl);
-        },
-    }
-  }).mount('#app')
+
+createApp({
+	data() {
+		return {
+			apiUrl: "http://localhost/php-dischi-json/database/server.php",
+			discs: [],
+		}
+	},
+	
+	methods: {
+		//!Chiamata GET per i dischi
+		getDiscs(url) {
+			axios.get(url)
+				.then((response) => {
+					console.log(response.data);
+					this.discs = response.data;
+				})
+				.catch(function (error) {
+					console.log(error);
+				});
+		},
+	},
+
+	//!Mostra i dischi
+	created() {
+		this.getDiscs(this.apiUrl);
+	},
+
+}).mount('#app')
