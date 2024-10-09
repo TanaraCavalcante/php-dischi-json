ad426f831054a457527ea8467da4baf8619ca839
TanaraCavalcante <tanaracavalcante@icloud.com>
2024-10-09 16:06:10 -0400

    api?

--- database/server.php
+++ database/server.php
@@ -1,1 +1,10 @@
-//! 
+<?php
+//! Prendo l'api 
+$apiDischi = file_get_contents("/api/dischi.json");
+echo $apiDischi;
+
+//! Risposta in Json
+// header('Content-Type: application/jason');
+
+//! Converto in json, se non è in json, per essere legibile in PHP
+// jason_encode()
